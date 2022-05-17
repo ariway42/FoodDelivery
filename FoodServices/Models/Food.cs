5cf830c4e20af3641762b575d27abcf07440cc21
Ariway42 <ariset780@gmail.com>
2022-05-17 09:56:40 +0700

    first commit

---
+++ FoodServices/Models/Food.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FoodServices.Models
+{
+    public partial class Food
+    {
+        public Food()
+        {
+            OrderDetails = new HashSet<OrderDetail>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public double Price { get; set; }
+
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
---
+++ UserServices/Models/OrderDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UserServices.Models
+{
+    public partial class OrderDetail
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int FoodId { get; set; }
+        public int Qty { get; set; }
+        public decimal Location { get; set; }
+        public decimal Tracker { get; set; }
+
+        public virtual Food Food { get; set; } = null!;
+        public virtual Order Order { get; set; } = null!;
+    }
+}
